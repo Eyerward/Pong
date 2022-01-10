3869decfce75b5df7ad63e5db3e9367df0fd66dc
Eyerward <matthieubruffa@gmail.com>
2022-01-10 15:27:32 +0100

    Update Tableau1.js

--- src/Tableau1.js
+++ src/Tableau1.js
@@ -15,16 +15,38 @@ class Tableau1 extends Phaser.Scene{
         this.hauteur = 500
         this.largeur = 1000
 
-        this.haut=this.add.tileSprite(0,0,this.largeur,20,'square').setOrigin(0,0);
+        this.haut=this.physics.add.image(0,0,'square').setOrigin(0,0);
+        this.haut.setDisplaySize(this.largeur,20);
+        this.haut.body.setAllowGravity(false);
+        this.haut.setImmovable(true);
 
-        this.bas=this.add.tileSprite(0,this.hauteur-20,this.largeur,20,'square').setOrigin(0,0);
+        this.bas=this.physics.add.image(0,this.hauteur-20,'square').setOrigin(0,0);
+        this.bas.setDisplaySize(this.largeur,20);
+        this.bas.body.setAllowGravity(false);
+        this.bas.setImmovable(true);
 
-
-        this.balle = this.add.image(this.largeur/2,this.hauteur/2,'ball').setOrigin(0,0);
+        this.balle = this.physics.add.image(this.largeur/2,this.hauteur/2,'ball').setOrigin(0,0);
         this.balle.setDisplaySize(20,20);
+        this.balle.body.setBounce(1.1,1.1);
+        this.balle.setVelocityX(Phaser.Math.Between(-200,200));
+        this.balle.body.setMaxVelocity(500,500);
+
+        this.physics.add.collider(this.balle,this.bas);
+        this.physics.add.collider(this.balle,this.haut);
     }
 
     update(){
-
+        if(this.balle.x > this.largeur){
+            this.balle.x = 0
+        }
+        if(this.balle.x < this.largeur){
+            this.balle.x = 500
+        }
+        if(this.balle.y < 0){
+            this.balle.y = 0
+        }
+        if(this.balle.y > this.hauteur){
+            this.balle.y = this.hauteur
+        }
     }
 }
